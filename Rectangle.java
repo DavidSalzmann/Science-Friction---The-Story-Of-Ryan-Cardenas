3589f06f3354beaf6cfb36d2b9d13d62cb2eeb29
Michael <34720626+seanarino@users.noreply.github.com>
2019-05-31 08:36:52 -0700

    the real rectangle

--- Rectangle.java
+++ Rectangle.java
@@ -1,4 +1,14 @@
 
 public class Rectangle {
 
+	public Rectangle(int i, int j, int k, int l) {
+		
+	}
+
+	Rectangle a = new Rectangle (150, 100, 100, 10);
+	Rectangle b = new Rectangle (300, 300, 100, 10);
+	Rectangle c = new Rectangle (450, 300, 300, 10);
+	Rectangle d = new Rectangle (800, 300, 125, 10);
+	Rectangle e = new Rectangle (950, 300, 150, 10);
+	Rectangle f = new Rectangle (1100, 300, 200, 10);
 }
