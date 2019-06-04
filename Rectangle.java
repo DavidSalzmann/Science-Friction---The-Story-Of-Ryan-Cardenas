7d076b6a385d5ad57981f565b78ee514b967ab8e
Michael <34720626+seanarino@users.noreply.github.com>
2019-06-04 08:44:05 -0700

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -10,4 +10,6 @@ public class Rectangle {
 	Rectangle d = new Rectangle (800, 650, 125, 10);
 	Rectangle e = new Rectangle (950, 550, 150, 10);
 	Rectangle f = new Rectangle (1100, 350, 200, 10);
+	Rectangle deathBox = new Rectangle (0, 1000, 1920, 80);
+
 }
