96cb8e7c240d1b0c459aa186a3521ba58cb0894a
Michael <34720626+seanarino@users.noreply.github.com>
2019-06-04 07:59:00 -0700

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,36 +1,13 @@
-package amazingPackageOfAmazingness;
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.geom.Ellipse2D;
-
-import javax.swing.*;
-
-
 public class Rectangle {
+
 	public Rectangle(int i, int j, int k, int l) {
-		
-		
+
 	}
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		Graphics2D g2 = (Graphics2D) g;
-		Rectangle rect = new Rectangle(x, y, z, q);
-		g.setColor(new Color(147, 112, 219));
-		g2.fill(rect);
 
-	
-	public void paint(Graphics g) {
-        g.fillRect(150, 100, 100, 10);
-        g.fillRect(300, 300, 100, 10);
-        g.fillRect(500, 300, 100, 10);
-        g.fillRect(700, 300, 100, 10);
-        g.fillRect(450, 300, 300, 10);
-        g.fillRect(900, 300, 400, 10);
-      //  g.fillRect(2000,300, 600, 10);
-        g.fillOval(2600, 300, 50, 50);
-        g.fillOval(1300, 250, 50, 50);
-
-    }
-	
+	Rectangle a = new Rectangle (105, 950, 100, 10);
+	Rectangle b = new Rectangle (300, 850, 100, 10);
+	Rectangle c = new Rectangle (450, 750, 300, 10);
+	Rectangle d = new Rectangle (800, 650, 125, 10);
+	Rectangle e = new Rectangle (950, 550, 150, 10);
+	Rectangle f = new Rectangle (1100, 350, 200, 10);
 }
